0b4538de43c35bbd10b083abf1507222adf9f76b
JDR <zhoude123>
2019-10-15 19:35:15 +0800

    eureka-client: 1、zuul、provider、consumer注册为eureka client

--- user-provider/src/main/java/com/jdr/maven/sc/integration/userprovider/UserProviderApplication.java
+++ user-provider/src/main/java/com/jdr/maven/sc/integration/userprovider/UserProviderApplication.java
@@ -2,8 +2,10 @@ package com.jdr.maven.sc.integration.userprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserProviderApplication {
 
     public static void main(String[] args) {
