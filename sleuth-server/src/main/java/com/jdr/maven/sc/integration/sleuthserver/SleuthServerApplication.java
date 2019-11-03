e1bebc285c292cb06648d42d611bf562abb0b954
JDR <zhoude@vcrecit.com>
2019-11-03 14:43:12 +0800

    zipkin: 1、zipkin持久化mysql+mq

--- sleuth-server/src/main/java/com/jdr/maven/sc/integration/sleuthserver/SleuthServerApplication.java
+++ sleuth-server/src/main/java/com/jdr/maven/sc/integration/sleuthserver/SleuthServerApplication.java
@@ -2,7 +2,11 @@ package com.jdr.maven.sc.integration.sleuthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import zipkin.server.internal.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableZipkinServer
@@ -12,4 +16,8 @@ public class SleuthServerApplication {
         SpringApplication.run(SleuthServerApplication.class, args);
     }
 
+    @Bean
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
+    }
 }
